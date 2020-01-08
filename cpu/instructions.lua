f55634740ee70b5a7c707f9c1e810ebc4564b71d
Ivy <adrisolgo7373@gmail.com>
2020-01-08 19:21:05 +0100

    Added TXS and TSX instructions and simplified flag detection

--- cpu/instructions.lua
+++ cpu/instructions.lua
@@ -1,5 +1,9 @@
 local bit = require "bit"
 
+-- Set flags according to the argument
+local function set_negative(byte) nes.cpu.N = bit.band(0x80, byte) == 0x80 and 1 or 0 end
+local function set_zero(byte) nes.cpu.Z = byte == 0x00 and 1 or 0 end
+
 local instructions = { 
     CLC = function() nes.cpu.C = 0 end,
     CLD = function() nes.cpu.D = 0 end,
@@ -8,13 +12,31 @@ local instructions = {
     LDA = function()
         local operand = nes.cpu.operand
         nes.cpu.A = operand
-        nes.cpu.N = bit.band(0x80, operand) == 0x80 and 1 or 0
-        nes.cpu.Z = operand == 0x00 and 1 or 0
+        set_negative(operand)
+        set_zero(operand)
+    end,
+    LDX = function()
+        local operand = nes.cpu.operand
+        nes.cpu.X = operand
+        set_negative(operand)
+        set_zero(operand)
     end,
     SEC = function() nes.cpu.C = 1 end,
     SED = function() nes.cpu.D = 1 end,
     SEI = function() nes.cpu.I = 1 end,
-    STA = function() nes.cpu:write(nes.cpu.operand, nes.cpu.A) end
+    STA = function() nes.cpu:write(nes.cpu.operand, nes.cpu.A) end,
+    TXS = function()
+        local X = nes.cpu.X
+        nes.cpu.SP = X
+        set_negative(X)
+        set_zero(X)
+    end,
+    TSX = function()
+        local SP = nes.cpu.SP
+        nes.cpu.X = SP
+        set_negative(SP)
+        set_zero(SP)
+    end
 }
 
 return instructions
--- cpu/opcodes.lua
+++ cpu/opcodes.lua
@@ -12,8 +12,11 @@ local opcodes = {
     [0x58] = opcode("CLI", "IMPLIED", 2),
     [0x78] = opcode("SEI", "IMPLIED", 2),
     [0x8d] = opcode("STA", "ABSOLUTE", 3),
+    [0x9a] = opcode("TXS", "IMPLIED", 2),
+    [0xa2] = opcode("LDX", "INMEDIATE", 2),
     [0xa9] = opcode("LDA", "INMEDIATE", 2),
     [0xb8] = opcode("CLV", "IMPLIED", 2),
+    [0xba] = opcode("TSX", "IMPLIED", 2),
     [0xd8] = opcode("CLD", "IMPLIED", 2),
     [0xf8] = opcode("SED", "IMPLIED", 2),
 }
