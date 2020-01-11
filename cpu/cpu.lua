19257c0943f32106defc03b999b4eeb435999153
Ivy <adrisolgo7373@gmail.com>
2020-01-11 13:32:29 +0100

    Stablished  the initial value of the SP for compatibility

--- cpu/cpu.lua
+++ cpu/cpu.lua
@@ -7,7 +7,7 @@ Cpu.__index = Cpu
 
 function Cpu:new()
     return setmetatable({
-        PC = 0, SP = 0, A = 0,
+        PC = 0, SP = 0xff, A = 0,
         X = 0, Y = 0, C = 0,
         Z = 0, I = 0, D = 0,
         B = 0, V = 0, N = 0,
